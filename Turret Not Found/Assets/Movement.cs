d1c648e19310d7c97688b1e7efbf0b97eae6220b
LeoVirolainen <virolainen.leo@gmail.com>
2019-06-11 20:28:14 +0300

    initial commit

---
+++ Turret Not Found/Assets/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed;
+    public float turningSpeed;
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.MovePosition(rb.position + rb.transform.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.MovePosition(rb.position - rb.transform.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            var dr = Quaternion.Euler(0, -turningSpeed * Time.deltaTime, 0);
+            rb.MoveRotation(rb.rotation * dr);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            var dr = Quaternion.Euler(0, turningSpeed * Time.deltaTime, 0);
+            rb.MoveRotation(rb.rotation * dr);
+        }
+    }
+}
