edc1ae9443625a3fa7af4063b8e55387b5f63d31
LeoVirolainen <virolainen.leo@gmail.com>
2019-06-16 16:56:21 +0300

    Skybox(es)

--- Turret Not Found/Assets/Scripts/TankController.cs
+++ Turret Not Found/Assets/Scripts/TankController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
+public class TankController : MonoBehaviour {
 
     public Rigidbody rb;
     public float speed;
